7ad8f1ad647081f14a7b367e996bb0e4ca778db6
Simon Kistler <skistler@kochag.ch>
2024-12-02 10:40:03 +0100

    day 1

--- lib/days/day_1.ex
+++ lib/days/day_1.ex
@@ -6,24 +6,38 @@ defmodule Aoc2024.Days.Day1 do
   alias Aoc2024.Helpers
 
   def solve(day) do
-    input = Helpers.read_input(day)
-    dbg input
+    input =
+      Helpers.read_input(day)
+      |> String.split("\n")
+      |> Enum.reduce([[], []], fn line, [list_a, list_b] ->
+        [a, b] = String.split(line)
+        {a, _} = Integer.parse(a)
+        {b, _} = Integer.parse(b)
+        [list_a ++ [a], list_b ++ [b]]
+      end)
+
     part1 = solve_part1(input)
     part2 = solve_part2(input)
     IO.puts("Part 1: #{part1}")
     IO.puts("Part 2: #{part2}")
   end
 
-  # Implement solutions for part 1 and part 2
-  defp solve_part1(input) do
-    # Your solution for part 1
-    input |> String.split("\n") |> Enum.map(&String.to_integer/1) |> Enum.sum()
+  defp solve_part1([list_a, list_b]) do
+    list_a = Enum.sort(list_a)
+    list_b = Enum.sort(list_b)
+
+    Enum.zip(list_a, list_b)
+    |> Enum.map(fn {a, b} -> abs(a - b) end)
+    |> Enum.sum()
   end
 
-  defp solve_part2(input) do
-    # Your solution for part 2
-    input |> String.split("\n") |> Enum.map(&String.to_integer/1) |> Enum.product()
+  defp solve_part2([list_a, list_b]) do
+    list_a
+    |> Enum.map(fn x ->
+      list_b
+      |> Enum.filter(fn y -> y == x end)
+      |> Enum.sum()
+    end)
+    |> Enum.sum()
   end
-
-  # Helper function to read input file
 end
